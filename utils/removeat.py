6c2e4c0abc2b8616cff401928d0589073db605a6
B.H Yoo <pikabh@naver.com>
2024-06-21 19:34:35 +0900

    20240621

--- utils/removeat.py
+++ utils/removeat.py
@@ -1,4 +1,30 @@
+# Internal Modules
+...
+# External Modules
 from typing import List
+import argparse
+import logging
+
+# Arguments
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('--file', '-F', default="requirements.txt", type=..., help='')
+parser.add_argument('--target', '-T', default='@', type=..., help='')
+args = parser.parse_args()
+
+# Root 
+logger_name = "utils.removeat"
+logger = logging.getLogger(logger_name)
+logger.setLevel(logging.DEBUG)
+# File Handler
+file_handler = logging.FileHandler(f'logs/{logger_name}.log', encoding='utf-8-sig')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(logging.Formatter(r'%(asctime)s [%(name)s, line %(lineno)d] %(levelname)s: %(message)s'))
+logger.addHandler(file_handler)
+# Stream Handler
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.INFO)
+stream_handler.setFormatter(logging.Formatter(r'%(message)s'))
+logger.addHandler(stream_handler)
 
 
 class MyString(str):
@@ -13,7 +39,8 @@ def rmrear(file_name : str, target : str) -> None:
 
 # main()
 def main() -> None:
-    rmrear("requirements.txt", "@")
+    rmrear(args.file, args.target)
+    logger.debug(f"Trop '{args.target}' from {args.file} successfully.")
 
 # Main()
 if __name__ == "__main__":
